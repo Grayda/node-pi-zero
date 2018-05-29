6a1c20b2977ccc226e31e3c3da5d3264ca716fc1
David Gray <grayda@solidinc.org>
2018-05-29 10:42:42 +1000

    Update code to use gulp 4, and correctly parse version numbers

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,25 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var fs = require("fs")
 var async = require("async")
-
+var semverSort = require("semver-sort")
 var http = require('http')
 var GetNodeVersions = require("get-node-versions").GetNodeVersions
 
-gulp.task('generate', function() {
+gulp.task('generate', function(done) {
   var validVersions = []
   var template = fs.readFileSync("./templates/install-node-template.txt", "utf8")
   fs.writeFileSync("README.md", fs.readFileSync("./templates/readme-template.txt", "utf8"))
   gutil.log("Requesting all node versions..")
   GetNodeVersions.parse(["all"]).then(function(versions) {
     async.map(versions, function(version, next) {
-      options = {method: 'HEAD', host: "nodejs.org", port: 80, path: '/dist/v' + version + "/node-v" + version + "-linux-" + architecture + ".tar.gz"}
+      options = {
+        method: 'HEAD',
+        host: "nodejs.org",
+        port: 80,
+        path: '/dist/v' + version + "/node-v" + version + "-linux-" + architecture + ".tar.gz"
+      }
       req = http.request(options, function(r, err) {
-        if(r.statusCode == "200") {
+        if (r.statusCode == "200") {
           gutil.log(gutil.colors.green("v" + version + " has an " + architecture + " build. Writing install script.."))
           validVersions.push(version)
           next(null, version)
@@ -29,12 +34,14 @@ gulp.task('generate', function() {
       });
       req.end();
     }, function() {
-      validVersions.sort().reverse().forEach(function(version) {
+      semverSort.desc(validVersions).forEach(function(version) {
         gutil.log("Writing " + version + " to file")
-        fs.writeFile("./install-node-v" + version + ".sh", template.replace("@@VERSION@@", version).replace("@@ARCH@@", architecture).replace("@@MIRROR@@", GetNodeVersions.NODEJS_MIRROR))
+        fs.writeFileSync("./install-node-v" + version + ".sh", template.replace("@@VERSION@@", version).replace("@@ARCH@@", architecture).replace("@@MIRROR@@", GetNodeVersions.NODEJS_MIRROR))
         fs.appendFileSync("README.md", ["## v" + version, "", "```sh", "$ wget https://raw.githubusercontent.com/" + username + "/node-pi-zero/master/install-node-v" + version + ".sh -O /tmp/install-node-v" + version + ".sh && source /tmp/install-node-v" + version + ".sh", "```", "", ""].join("\n"))
       })
       gutil.log(gutil.colors.cyan("Wrote " + validVersions.length + " versions"))
+      done()
     })
   })
+
 })
